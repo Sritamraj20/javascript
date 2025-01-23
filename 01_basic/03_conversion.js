eb3bba7871eb8ace40a636e2b6032465f20eccc8
Sritamraj Mahakul <97335911+Sritamraj20@users.noreply.github.com>
2025-01-23 09:51:34 +0000

    conversion in js

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,48 @@
+let a="10"
+
+let updateNumber=Number(a)
+
+console.log(updateNumber)
+
+let b="33ab"
+
+let alphaNumeric=Number(b)
+
+console.log(alphaNumeric)
+console.log(typeof alphaNumeric)
+
+let c=null
+let nullcheck=Number(c)
+
+console.log(typeof nullcheck)
+console.log(nullcheck)
+
+let d=undefined
+let undefinedCheck=Number(d)
+
+console.log(typeof undefinedCheck)
+console.log(undefinedCheck)
+
+let e=""
+let blankValue=Number(e)
+
+console.log(typeof blankValue)
+console.log(blankValue)
+
+let f="sritam"
+let withValue=Number(f)
+
+console.log(typeof withValue)
+console.log(withValue)
+
+let g=1
+let booleanValue=Boolean(g)
+
+console.log(typeof booleanValue)
+console.log(booleanValue)
+
+let h=23
+let stringValue=String(h)
+
+console.log(typeof stringValue)
+console.log(stringValue)
