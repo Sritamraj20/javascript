fef646150525aa01e5dbca24b18a8c3a909a2598
Sritamraj Mahakul <97335911+Sritamraj20@users.noreply.github.com>
2025-01-25 06:29:33 +0000

    topic on equalitycheck and 05_comparisons

---
+++ 01_basic/05_comparisons.js
@@ -0,0 +1,8 @@
+console.log("2">1)//convert it to int then compare so value is true
+console.log("02">1)
+
+console.log(null>0)//false
+console.log(null==0)//false
+console.log(null>=0)//true
+
+/* here in above illustration we have seen in js when the comparison operator is being used then the value convert to numerical(null=0) but for equalcheck the case is different)*/
