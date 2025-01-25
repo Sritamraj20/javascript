47fb9c760683873bae76305efe1e12678656d5bd
Sritamraj Mahakul <97335911+Sritamraj20@users.noreply.github.com>
2025-01-25 06:16:06 +0000

    topic on prefix and postfix and operations

---
+++ 01_basic/04_operations.js
@@ -0,0 +1,30 @@
+let value=3
+let negValue=-value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2/2)
+console.log(2%2)
+
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log("1"+2)
+console.log(1+"2")
+
+let str1="hello"
+let str2=" sritam"
+
+console.log(str1+str2)
+
+console.log(true)
+console.log(+true)//don't use this type of code
+
+let num1=3,num2=4;
+let num3,num4,num5=4;
+
+let gameCounter=100
+let postfix=gameCounter++
+let prefix=++gameCounter
+console.log(prefix+" "+postfix)
