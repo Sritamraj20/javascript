8d3a79b979d76a63ce5c5c594783e08aa7444d03
Sritamraj Mahakul <97335911+Sritamraj20@users.noreply.github.com>
2025-01-26 07:57:41 +0000

    topic on dateAndTime in js

---
+++ 01_basic/10_dates.js
@@ -0,0 +1,9 @@
+const newDate=new Date()
+console.log(newDate)
+
+console.log(newDate.getDate())
+console.log(newDate.getDay())
+console.log(newDate.getFullYear())
+console.log(newDate.getHours())
+console.log(newDate.getMinutes())
+console.log(newDate.getMonth())
