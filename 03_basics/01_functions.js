ab6830ac2d9ef1214af533b0ead98e4e3fbbe1ec
Sritamraj Mahakul <97335911+Sritamraj20@users.noreply.github.com>
2025-01-26 11:46:29 +0000

    functions in js

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,26 @@
+function sayMyName(){//syntax
+    console.log("s")
+}
+sayMyName()
+
+function addition(number1,number2){
+    return number1+number2
+}
+// let n=addition(1,2)
+// console.log(n)
+
+console.log(addition("sritam","raj"))
+
+
+
+const s=addition(1,2)
+console.log("result:a"+s)
+console.log("result:",s)
+
+//String Concatenation (+ operator) directly combines the string and the value, forming a single string output.
+//Comma Separation (,) logs multiple arguments separately, and it will add a space between them in the console.
+
+const add=function sub(num1,num2){
+    return num1-num2
+}
+console.log(add(5,3))//This is how we call function with declaration.
