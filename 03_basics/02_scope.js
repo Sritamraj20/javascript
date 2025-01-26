dcfb006e3b2e3476ce1aaab395675aaccab75de8
Sritamraj Mahakul <97335911+Sritamraj20@users.noreply.github.com>
2025-01-26 18:28:21 +0000

    indepth scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -27,3 +27,42 @@ if(true){
 console.log(a)//global scope
 console.log(b)//global scope
 console.log(c)//as c is of var type then we can change at anypoint and it will return recent value.
+
+function scope(){
+    let a="scope"
+
+    function insideScope(){
+        let inside="scopeinside"
+        console.log(a)
+        console.log(inside)
+    }
+    //console.log(inside);gives error as it's out of scope.
+    insideScope()
+    
+}
+scope()
+
+
+//if-else scope
+
+if(true){
+    let highest="himalaya"
+    if(highest==="himalaya"){
+        console.log(highest);
+        let lowset="alps"
+    }
+    //console.log(lowset);//out of scope
+
+}
+//console.log(highest);
+
+
+pehleAccess()//without even function signature here i am able to access the function.
+function pehleAccess(){
+    console.log("I can access pehle"); 
+}
+
+// dekhtahu()//can't access before initialization
+// const dekhtahu=function nehniHoRahaHai(){
+//     console.log("I can't access before");
+// }
