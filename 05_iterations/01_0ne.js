2542b2b43bba7139d7a4da5262f027f892fed27d
Sritamraj Mahakul <97335911+Sritamraj20@users.noreply.github.com>
2025-01-27 15:25:26 +0000

    iterations

--- 05_iterations/01_0ne.js
+++ 05_iterations/01_0ne.js
@@ -1,1 +1,31 @@
-let a=10
+// const arr=[1,2,3,4]
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+//     if(arr[i]==3){
+//         //break
+//         continue
+//     }
+// }
+
+// for(;;){
+//     console.log("I am here");
+// } run infinity
+
+// for(let index=0;index<10;){
+//     console.log("looping")
+//     index++
+// }
+
+//do-while
+let a=0;
+do{
+    console.log("do-while")
+    a++
+}while(a<1)
+
+// let b=0
+// while(b<=10){
+//     console.log("inside while loop");
+//     b+=2
+// }
